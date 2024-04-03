31d16c9e7c69957fa8de3fb03a229031df086388
DPDev <130209471+DPrpDev@users.noreply.github.com>
2024-04-03 20:36:59 +1030

    v0.1
    Unfished

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version "cerulean"
+game "gta5"
+
+title "LB PS Groups App"
+description "PS Groups Converted for LB-Phone App"
+author "Don Panino"
+
+client_script "client.lua"
+server_script "server.lua"
+
+files {
+    "ui/**/*"
+}
+
+ui_page "ui/index.html"
